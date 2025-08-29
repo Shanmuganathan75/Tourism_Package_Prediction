ec1f75fe658fb2fcf5fe2580ac23262e577b351a
Shanmuganathan Ekambaram <Shan_muganathan@yahoo.com>
2025-08-29 22:53:18 +0530

    Update app.py

--- 01) MLOPS/03) DEPLOYMENT/app.py
+++ 01) MLOPS/03) DEPLOYMENT/app.py
@@ -7,16 +7,13 @@ import joblib
 model_path = hf_hub_download(repo_id="Shanmuganathan75/Tourism-Package-Prediction", filename="best_tourism_prediction_model_v1.joblib")
 model = joblib.load(model_path)
 
-# Streamlit UI for Tourism  Package Prediction
-st.title("Tourism Package Prediction App")
+# --- Streamlit Inputs ---
+st.title("Tourism Package Prediction")
 st.write("""
 This application predicts the likelihood of a Package selection prediction.
 Please enter the   data below to get a prediction.
 """)
-
-# --- Streamlit Inputs ---
-st.title("Tourism Package Prediction")
-col1, col2 = st.columns(2)
+col1, col2, Col3 = st.columns(3)
 with col1:
 # Categorical inputs
   TypeofContact = st.selectbox("Type of Contact ", ["Company Invited", "Self Enquiry"])
@@ -25,14 +22,15 @@ with col1:
   ProductPitched = st.selectbox("Product Pitched ", ["Basic","Deluxe","King","Standard","Super Deluxe"])
   MaritalStatus = st.selectbox("Marital Status ", ["Single","Married","Unmarried","Divorced"]) 
   Designation = st.selectbox("Designation ", ["VP","AVP","Manager","Senior Manager","Executive"])
-# Numerical inputs
+with col2:
+  # Numerical inputs
   Age = st.number_input("Age", min_value=0, max_value=100, value=40, step=1)  
   CityTier = st.number_input("City Tier", min_value=1, max_value=3, value=2, step=1)  
   DurationOfPitch = st.number_input("Duration of Pitch", min_value=1, max_value=1000, value=20, step=1)
-with col2:
   NumberOfPersonVisiting = st.number_input("Number of Person Visiting", min_value=1, max_value=10, value=2, step=1) 
   NumberOfFollowups = st.number_input("Number of Followups", min_value=1, max_value=10, value=2, step=1) 
   PreferredPropertyStar = st.number_input("Preferred Property Star", min_value=1, max_value=5, value=2, step=1) 
+with col3:
   NumberOfTrips = st.number_input("Number of Trip", min_value=1, max_value=10, value=2, step=1) 
   Passport = st.number_input("Passport Availability", min_value=0, max_value=1, value=1, step=1) 
   PitchSatisfactionScore = st.number_input("Pitch Satisfaction Score", min_value=1, max_value=5, value=3, step=1) 
