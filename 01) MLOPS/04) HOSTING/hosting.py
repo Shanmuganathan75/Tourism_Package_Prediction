0f380d46555e98d2cfc95fc77c4a5320ceec36e9
Shanmuganathan Ekambaram <Shan_muganathan@yahoo.com>
2025-08-29 21:15:55 +0530

    Update hosting.py

--- 01) MLOPS/04) HOSTING/hosting.py
+++ 01) MLOPS/04) HOSTING/hosting.py
@@ -1,9 +1,9 @@
 from huggingface_hub import HfApi
 import os
 
-api = HfApi(token=os.getenv("HF_TOKEN"))
+api = HfApi(token=os.getenv("HGF_TOKEN"))
 api.upload_folder(
-    folder_path="01) MLOPS/03) Deployment",                                    # the local folder containing your files
+    folder_path="01) MLOPS/03) DEPLOYMENT",                                    # the local folder containing your files
     repo_id="Shanmuganathan75/Tourism-Package-Prediction",                     # the target repo
     repo_type="space",                                                         # dataset, model, or space
     path_in_repo="",                                                            # optional: subfolder path inside the repo
