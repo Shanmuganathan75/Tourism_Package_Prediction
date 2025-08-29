ce7b452026b8576f0d4fa203421c529b0bd6f8ca
Shanmuganathan Ekambaram <Shan_muganathan@yahoo.com>
2025-08-29 22:39:34 +0530

    Update app.py

--- 01) MLOPS/03) DEPLOYMENT/app.py
+++ 01) MLOPS/03) DEPLOYMENT/app.py
@@ -16,28 +16,29 @@ Please enter the   data below to get a prediction.
 
 # --- Streamlit Inputs ---
 st.title("Tourism Package Prediction")
-
+col1, col2 = st.columns(2)
+with col1:
 # Categorical inputs
-TypeofContact = st.selectbox("Type of Contact ", ["Company Invited", "Self Enquiry"])
-Occupation = st.selectbox("Occuptaion ", ["Salaried","Free Lancer","Small Business","Large Business"]) 
-Gender = st.selectbox("Gender", ["Male", "Female"])
-ProductPitched = st.selectbox("Product Pitched ", ["Basic","Deluxe","King","Standard","Super Deluxe"])
-MaritalStatus = st.selectbox("Marital Status ", ["Single","Married","Unmarried","Divorced"]) 
-Designation = st.selectbox("Designation ", ["VP","AVP","Manager","Senior Manager","Executive"])
-
+  TypeofContact = st.selectbox("Type of Contact ", ["Company Invited", "Self Enquiry"])
+  Occupation = st.selectbox("Occuptaion ", ["Salaried","Free Lancer","Small Business","Large Business"]) 
+  Gender = st.selectbox("Gender", ["Male", "Female"])
+  ProductPitched = st.selectbox("Product Pitched ", ["Basic","Deluxe","King","Standard","Super Deluxe"])
+  MaritalStatus = st.selectbox("Marital Status ", ["Single","Married","Unmarried","Divorced"]) 
+  Designation = st.selectbox("Designation ", ["VP","AVP","Manager","Senior Manager","Executive"])
 # Numerical inputs
-Age = st.number_input("Age", min_value=0, max_value=100, value=40, step=1)  
-CityTier = st.number_input("City Tier", min_value=1, max_value=3, value=2, step=1)  
-DurationOfPitch = st.number_input("Duration of Pitch", min_value=1, max_value=1000, value=20, step=1)
-NumberOfPersonVisiting = st.number_input("Number of Person Visiting", min_value=1, max_value=10, value=2, step=1) 
-NumberOfFollowups = st.number_input("Number of Followups", min_value=1, max_value=10, value=2, step=1) 
-PreferredPropertyStar = st.number_input("Preferred Property Star", min_value=1, max_value=5, value=2, step=1) 
-NumberOfTrips = st.number_input("Number of Trip", min_value=1, max_value=10, value=2, step=1) 
-Passport = st.number_input("Passport Availability", min_value=0, max_value=1, value=1, step=1) 
-PitchSatisfactionScore = st.number_input("Pitch Satisfaction Score", min_value=1, max_value=5, value=3, step=1) 
-OwnCar = st.number_input("Own Car", min_value=0, max_value=1, value=1, step=1) 
-NumberOfChildrenVisiting = st.number_input("Number of Children Visiting", min_value=1, max_value=5, value=3, step=1)
-MonthlyIncome = st.number_input("Monthly Income", min_value=100, max_value=300000, value=15000, step=100)  
+  Age = st.number_input("Age", min_value=0, max_value=100, value=40, step=1)  
+  CityTier = st.number_input("City Tier", min_value=1, max_value=3, value=2, step=1)  
+  DurationOfPitch = st.number_input("Duration of Pitch", min_value=1, max_value=1000, value=20, step=1)
+with col2:
+  NumberOfPersonVisiting = st.number_input("Number of Person Visiting", min_value=1, max_value=10, value=2, step=1) 
+  NumberOfFollowups = st.number_input("Number of Followups", min_value=1, max_value=10, value=2, step=1) 
+  PreferredPropertyStar = st.number_input("Preferred Property Star", min_value=1, max_value=5, value=2, step=1) 
+  NumberOfTrips = st.number_input("Number of Trip", min_value=1, max_value=10, value=2, step=1) 
+  Passport = st.number_input("Passport Availability", min_value=0, max_value=1, value=1, step=1) 
+  PitchSatisfactionScore = st.number_input("Pitch Satisfaction Score", min_value=1, max_value=5, value=3, step=1) 
+  OwnCar = st.number_input("Own Car", min_value=0, max_value=1, value=1, step=1) 
+  NumberOfChildrenVisiting = st.number_input("Number of Children Visiting", min_value=1, max_value=5, value=3, step=1)
+  MonthlyIncome = st.number_input("Monthly Income", min_value=100, max_value=300000, value=15000, step=100)  
 
 
 # --- Build DataFrame in exact order expected by model ---
